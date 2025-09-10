9fe5c3d480ef1e630127b43ecbe22f66fc66693a
kashifashah1 <kashifa.shah24@gmail.com>
2025-09-10 08:50:31 +0530

    Password Manager Project Added

---
+++ passop/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Manager/>
+    </div>
+  )
+}
+
+export default App
